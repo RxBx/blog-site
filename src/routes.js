2365769efce3d7c83a7ead677baf26825840752c
RxBx <rxbabcock@yahoo.com>
2016-07-26 14:53:57 -0700

    remove demo code

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,6 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 
-const Greeting = () => {
-	return <div>Hey There!</div>;
-};
-
 //Routes can nest in a Route
 //IOW google.com/ ...renders: App
 //IOW google.com/greet2 ...renders: App, Greeting
@@ -16,9 +12,6 @@ const Greeting = () => {
 export default (
 	<Route path="/" component={App} >
 	<IndexRoute component={PostsIndex} />
-		<Route path="greet" component={Greeting} />
-		<Route path="greet2" component={Greeting} />
-		<Route path="greet3" component={Greeting} />
 	</Route>
 );
 
