90ba6f0e7ab1fb9f96d6def7a85b53be6044a7db
RxBx <rxbabcock@yahoo.com>
2016-07-27 16:53:37 -0700

    implement validate function for flagging incomplete form elements, stopping submit via redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -14,25 +14,53 @@ class PostsNew extends Component {
 		return (
 			<form onSubmit={handleSubmit(this.props.createPost)}>
 				<h3>Create a New Post</h3>
-				<div className="form-group">
+				<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
 					<label>Title</label>
 					<input type="text" className="form-control" {...title} />
+					<div className="text-help">
+						{title.touched ? title.error : ''}
+					</div>
 				</div>
 
-				<div className="form-group">
+				<div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
 					<label>Categories</label>
 					<input type="text" className="form-control" {...categories} />
+					<div className="text-help">
+						{categories.touched ? categories.error : ''}
+					</div>
 				</div>
 
-				<div className="form-group">
+				<div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
 					<label>Content</label>
 					<textarea type="text" className="form-control" {...content} />
 				</div>
+				<div className="text-help">
+						{content.touched ? content.error : ''}
+					</div>
 
 				<button type="submit" className="btn btn-primary">Submit</button>
 			</form>
 		);
 	}
+}
+
+//validation
+function validate(values) {
+	const errors = {};
+	if (!values.title) {
+		//below somehow gets attached to title.error in form's error div above?
+		errors.title = 'Enter a username';
+	}
+	if (!values.categories) {
+		//below somehow gets attached to title.error in form's error div above?
+		errors.categories = 'Enter categories';
+	}
+	if (!values.content) {
+		//below somehow gets attached to title.error in form's error div above?
+		errors.content = 'Enter a post message';
+	}
+
+	return errors;
 }
 
 // w old connect args are: mapStateToProps, mapDispatchToProps
@@ -44,5 +72,6 @@ class PostsNew extends Component {
 //by using reduxForm, and without using connect/dispatch from 'react-redux'
 export default reduxForm({
 	form: 'PostsNewForm',
-	fields: ['title', 'categories', 'content']
+	fields: ['title', 'categories', 'content'],
+	validate
 }, null, { createPost })(PostsNew);
