2052bac18a710f1d2b2e831c80c08d7b5a0c9a3d
RxBx <rxbabcock@yahoo.com>
2016-07-28 15:10:10 -0700

    create auto change after delete back  to index using PropTypes/router/then/push chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
 const API_KEY = '?key=doctorloctor';
@@ -15,7 +16,6 @@ export function fetchPosts () {
 	return {
 		type: FETCH_POSTS,
 		payload: request
-
 	};
 }
 
@@ -26,7 +26,6 @@ export function createPost(props) {
 	return {
 		type: CREATE_POST,
 		payload: request
-
 	};
 
 }
@@ -37,7 +36,16 @@ export function fetchPost(id) {
 	return {
 		type: FETCH_POST,
 		payload: request
+	};
 
+}
+
+export function deletePost(id) {
+	const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+	return {
+		type: DELETE_POST,
+		payload: request
 	};
 
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+	//context for post-delete navigation
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
 	//params.id is being supplied by the URL, 
 	componentWillMount() {
 		this.props.fetchPost(this.props.params.id);
+	}
+
+	onDeleteClick() {
+		this.props.deletePost(this.props.params.id)
+			.then(() => {
+				//blog post has been create, navigate user to index
+				// we navigate4 by calling this.context.router.push with the
+				//new path to navigate to.
+				this.context.router.push('/');
+		});
 	}
 
 
@@ -19,10 +35,17 @@ class PostsShow extends Component {
 		}
 		return (
 			<div>
+				<Link to="/" >Back to Index</Link>
+				<button 
+					className="btn btn-danger pull-xs-right"
+					onClick={this.onDeleteClick.bind(this)}>
+					Delete Post
+				</button>
 				<h3>{post.title}</h3>
 				<h6>Categories: {post.categories}</h6>
 				<p>{post.content}</p>
 			</div>
+
 		);
 	}
 }
@@ -31,4 +54,4 @@ function mapStateToProps(state) {
 	return {post: state.posts.post }
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, {fetchPost, deletePost })(PostsShow);
